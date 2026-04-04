8416aa14a4f0c4c8dbb367464e76a890da095ee7
keerti hatti <keertihattik@gmail.com>
2026-04-04 23:23:30 +0530

    Update queries.sql
    Added SQL queries used in project

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,26 +1,26 @@
--- 1. Total sales by region
+1. Total sales by region
 SELECT Region, SUM(Sales) AS Total_Sales
 FROM sales_data
 GROUP BY Region;
 
--- 2. Total profit by region
+2. Total profit by region
 SELECT Region, SUM(Profit) AS Total_Profit
 FROM sales_data
 GROUP BY Region;
 
--- 3. Top selling product
+3. Top selling product
 SELECT Product, SUM(Sales) AS Total_Sales
 FROM sales_data
 GROUP BY Product
 ORDER BY Total_Sales DESC
 LIMIT 5;
 
--- 4. Sales by category
+4. Sales by category
 SELECT Category, SUM(Sales) AS Total_Sales
 FROM sales_data
 GROUP BY Category;
 
--- 5. Average discount by region
+5. Average discount by region
 SELECT Region, AVG(Discount) AS Avg_Discount
 FROM sales_data
 GROUP BY Region;
